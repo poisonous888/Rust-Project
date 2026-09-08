ef4a50de46ba89b9aaadf1ef18cf9b8dbdf8a781
poisonous888 <rwlittle888@gmail.com>
2026-09-07 20:58:49 -0400

    initial framework

---
+++ src/main.rs
@@ -0,0 +1,15 @@
+use rand;
+use std::io;
+
+fn main() {
+    println!("Hello, world!");
+    let random_test = rand::random::<i32>();
+    println!("{:?}", random_test);
+    let input = io::stdin();
+    let mut guess = String::new();
+    input.read_line(&mut guess).expect("TODO: panic message");
+    println!("{}", guess);
+    if 1>2 {
+        
+    }
+}
